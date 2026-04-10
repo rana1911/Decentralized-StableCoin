1c055071174102220628887165a1c10aff8302e4
Rana Suryam <ranasuryam19@gmail.com>
2026-04-10 21:55:23 +0530

    Initial commit

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.33;
+pragma solidity ^0.8.33;
 
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.33;
+pragma solidity ^0.8.33;
 
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
